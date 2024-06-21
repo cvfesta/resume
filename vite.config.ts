c816546e99d4456274cb88d9977a944c5e7ffb63
cvfesta <97863147+cvfesta@users.noreply.github.com>
2024-06-21 15:28:11 -0700

    revert vite config   base: '/resume-react/',

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vite-project/',
+  base: '/resume-react/',
   plugins: [react()],
 });

901f215fef17ba109723fac5e2102b46948514c2
cvfesta <97863147+cvfesta@users.noreply.github.com>
2024-06-21 16:39:14 -0700

    update index path to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,8 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
-
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )

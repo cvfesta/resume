47592209cdd5200c599a1397228f850ba16b28be
cvfesta <97863147+cvfesta@users.noreply.github.com>
2024-06-21 16:10:46 -0700

    updated main function

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,14 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )

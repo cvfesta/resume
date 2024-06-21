c207b0b577fb6e85977c883fbbec510442789b76
cvfesta <97863147+cvfesta@users.noreply.github.com>
2024-06-21 16:45:34 -0700

    added build property for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/resume-react/"
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/resume-react/"
+  base: "/resume-react/",
+  build: {
+    outDir: '', // This outputs files to the root directory
+  },
 });

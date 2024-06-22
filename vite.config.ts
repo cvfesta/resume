a307713bc36d1b43b02f84c7a73e486974f02676
cvfesta <97863147+cvfesta@users.noreply.github.com>
2024-06-22 10:06:53 -0700

    clean up

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,3 @@ export default defineConfig({
   plugins: [react()],
   base: "/resume-react/"
 });
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/resume-react/",
-//   build: {
-//     outDir: 'dist', // Create a 'dist' directory for built files
-//   },
-// });
